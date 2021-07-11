c4594675ded80c22ea3422566d2292e21f6f6a11
Paweł Betnerowicz <43805630+betnero@users.noreply.github.com>
2021-07-11 17:03:57 +0200

    deciamltobinary.py
    A decimal to binary converter with simple user input validation and program restart.

---
+++ python/decimaltobinary.py
@@ -0,0 +1,24 @@
+while True: # The loop together with the go_again variable will enable program restart at users will
+    
+    try: # try: except: mechanism for user input validation
+        print() # Print empty line
+        usr = input("What number?: ") # User input
+        print('{0:08b}'.format( int(usr))) # Binary represntation of user input
+        print()
+
+        # Print user input length
+        print("Number digits entered: ")
+        print(len(usr))
+        print()
+
+        # Print length of the binary convertion
+        print("Number bits in the number (min.8): ")
+        print(len('{0:08b}'.format(int(usr))))
+        print()
+        
+    except:
+        print("That's not a number. Please enter a number.") # User prompt
+
+    go_again = input("Want to do another one?... (y/n)")
+    if go_again != "y":
+        break
