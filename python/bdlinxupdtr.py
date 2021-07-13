e63f2f538a95fcc3078fc6b98b8cfe911aa17c66
Paweł Betnerowicz <43805630+betnero@users.noreply.github.com>
2021-07-13 18:42:31 +0200

    Linux upadte and upgrade script
    Not very usable. Python is too slow for that. PoC result == can be done but pointless.

---
+++ python/bdlinxupdtr.py
@@ -0,0 +1,20 @@
+#!/bin/env python3
+# This script is suppose to update and upgrade Linux but is slow. Done purely as a PoC. Better off doing updates manually or use a bash script.
+
+from subprocess import call
+
+
+call(["sudo", "apt-get", "update"])
+print("\n")
+print("-."*50)
+print('                         ***  UPDATE DONE...  ***')
+print("-."*50)
+print("\n")
+print("-."*50)
+call(["sudo", "apt-get", "upgrade", "-y"])
+print("-."*50)
+print("\n")
+print("-."*50)
+print('                         ***  UPDATE DONE...  ***')
+print("-."*50)
+print("\n")
