ae4b583a7195d7ea25748b8828adf6c08e57c437
Paweł Betnerowicz <43805630+betnero@users.noreply.github.com>
2021-07-26 13:16:08 +0200

    Loops in C
    Loops in C.

---
+++ C_C++/looping.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main(void)
+{
+    int a = 0;
+
+    while (a < 5)
+    {
+        printf("a is eqaul to %d\n", a);
+        a++;
+    }
+    printf ("a is eqaul to %d and I've finished\n", a);
+    
+}
