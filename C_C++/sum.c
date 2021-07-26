58d28e8c9bc8bce301652fd662ee708418b3f7f6
Paweł Betnerowicz <43805630+betnero@users.noreply.github.com>
2021-07-26 13:23:30 +0200

    sum.c
    Sum of numbers in C.

---
+++ C_C++/sum.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void main(void)
+{
+    int a;
+    int b = 3;
+    int c;
+
+    a = 2;
+    c = a + b;
+    printf("The sum of adding %d and %d is %d\n", a, b, c);
+}
+/*char stores values from -127 to 128; unsigned char stores positive values from 0 to 256*/
