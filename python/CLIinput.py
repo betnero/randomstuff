fcead4d506642e2975f74fe8110a98e72c5e134a
betner <betner@debian-BULLSEYE-live-builder-AMD64>
2022-01-15 13:37:19 +0100

    minor changes

---
+++ python/CLIinput.py
@@ -0,0 +1,14 @@
+#!/bin/env python3
+
+#import sys
+import os
+def updt():
+  os.system('sudo apt-get update')
+  os.system('sudo apt-get upgrade -y')
+  os.system('sudo apt update')
+  os.system('sudo apt-get autoremove')
+  os.system('sudo apt-get autoclean')
+  os.system('sudo apt-get clean')
+  os.system('sudo apt-get dist-upgrade')
+
+updt()
