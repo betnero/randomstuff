66a998751fa12d4ebc9ae42e907ac665a672746d
betnero <pawel.betnerowicz@gmail.com>
2022-01-15 21:03:48 +0100

    Re-write the script to take user choice of interface

--- bash/randomacchanger.sh
+++ bash/randomacchanger.sh
@@ -1,9 +1,26 @@
-#1/bin/bash
-service NetworkManager stop
-ifconfig eth0 down
-macchanger -r eth0
-ifconfig eth0 up
-ifconfig wlan0 down
-macchanger -r wlan0
-ifconfig wlan0 up
-service NetworkManager start
+#/bin/bash
+
+#the script launches macchange and changes MAC of a chosen interface to a random value
+
+echo " "
+sepearator="--------------------------------------------"
+echo "These are your interfaces: " 
+eval "ls /sys/class/net/" #list folders -- their names represent names of the intefaces on the machine
+
+echo $sepearator
+
+read -p "Enter interface name: " intrf # intrf is interface name variable
+
+echo $sepearator
+
+#number of macchanger commands normally entered manually
+eval "service NetworkManager stop"
+eval "ifconfig $intrf down" 
+eval "macchanger -r $intrf"
+eval "ifconfig $intrf up"
+eval "service NetworkManager start"
+
+echo $sepearator
+
+echo "MAC for $intrf changed..."
+echo " "
