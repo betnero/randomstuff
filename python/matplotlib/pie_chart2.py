246f07cbf2018bf42adefa5f8defecd02c0ed747
Paweł Betnerowicz <43805630+betnero@users.noreply.github.com>
2022-02-08 18:45:04 +0100

    init commit
    Init commit matplotlib piechart example.

---
+++ python/matplotlib/pie_chart2.py
@@ -0,0 +1,11 @@
+#!/bin/env python3
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+y = np.array([35, 25, 25, 15])
+mylabels = ["Apples", "Bananas", "Cherries", "Dates"]
+
+plt.pie(y, labels = mylabels)
+plt.legend(title = "Four Fruits:")
+plt.show() 
